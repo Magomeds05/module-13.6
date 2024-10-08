29e8f5886c98996ef9e2d99e2506042463ebfaae
Magomeds05 <yusypovm@gmail.com>
2024-10-08 14:19:00 +0300

    Update module 13.6.py

--- module 13.6.py
+++ module 13.6.py
@@ -6,7 +6,7 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.dispatcher import FSMContext
 import asyncio
 
-api = "8197570567:AAF2r-nMMyE_nqJ4Ll3dAHgXvzRn5kNVegw"
+api = ""
 bot = Bot(token= api)
 disp = Dispatcher(bot, storage=MemoryStorage())
 
